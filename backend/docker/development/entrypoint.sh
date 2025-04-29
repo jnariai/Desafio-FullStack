f93f7485ac36b61a214728c095feb68bfe823c06
Jonathan Nariai <jonathannariai@gmail.com>
2025-04-29 00:04:10 -0300

    chore: update environment configuration and entrypoint script

--- backend/docker/development/entrypoint.sh
+++ backend/docker/development/entrypoint.sh
@@ -3,6 +3,11 @@
 composer install
 npm install
 
+if [ ! -f .env ]; then
+    cp .env.example .env
+    php artisan key:generate
+fi
+
 php artisan migrate --force &
 php artisan optimize:clear &
 npm run dev -- --host 0.0.0.0 &
