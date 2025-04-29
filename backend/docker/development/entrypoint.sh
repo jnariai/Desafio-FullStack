6b3a07c22d371011b5f7baa86cfee93f97a2518b
Jonathan Nariai <jonathannariai@gmail.com>
2025-04-29 00:19:24 -0300

    chore: ensure storage directory for views exists in entrypoint script

--- backend/docker/development/entrypoint.sh
+++ backend/docker/development/entrypoint.sh
@@ -8,6 +8,12 @@ if [ ! -f .env ]; then
     php artisan key:generate
 fi
 
+if [ ! -d storage/framework/views ]; then
+    mkdir -p storage/framework/views
+    chmod -R 775 storage/framework/views
+fi
+
+
 php artisan migrate --force &
 php artisan optimize:clear &
 npm run dev -- --host 0.0.0.0 &
